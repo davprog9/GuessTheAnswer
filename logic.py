3cdccf5a59fb92008fe73eaa30ecc919eae2d81f
davprog <security9990@mail.ru>
2023-01-02 18:26:33 -0500

    Added file_opener method and read_next_line method for file reading

--- logic.py
+++ logic.py
@@ -7,8 +7,19 @@ class Logic:
         self.file_path = file_path
         self.max_misses = max_misses
         self.game_status = GameStatus.IN_PROGRESS
-        self.name = "John"
+        self.counter = 0
 
+    def file_opener(self):
+        """Opens the file, and returns the file"""
+        file = open(self.file_path, "r")
+        return file
 
+    def read_next_line(self, file):
+        """
+        Reads file's next line - by splitting it into a list of texts
+        Each text contains either a question or an answer
 
+        """
+        next_line = file.readline().split(";")
 
+        return next_line
