ddfae673515281002faade4b8098ee095420b3cb
davprog <david.arzm9@gmail.com>
2023-01-01 21:29:07 -0500

    Created a class with constructor

---
+++ logic.py
@@ -0,0 +1,12 @@
+from game_status import GameStatus
+
+
+class Logic:
+
+    def __init__(self, file_path, max_misses):
+        self.file_path = file_path
+        self.max_misses = max_misses
+        self.game_status = GameStatus.IN_PROGRESS
+
+
+
