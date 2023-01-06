ed98f54b2d35ccfe83e20bb8deea0d5fa210dd17
davprog <security9990@mail.ru>
2023-01-06 00:20:22 -0500

    Added generate_question method and a few docstrigs for methods

--- logic.py
+++ logic.py
@@ -1,6 +1,7 @@
 from game_status import GameStatus
 from question import Question
 
+
 class Logic:
 
     def __init__(self, file_path, max_misses):
@@ -13,7 +14,12 @@ class Logic:
 
     @property
     def game_status(self):
-        return self.game_status
+        """
+        Returns the game status from game_status.py
+        :return: Game status
+        """
+
+        return self.__game_status
 
     def fill_in_questions(self, file_path, questions):
         with open(file_path, encoding="utf8") as file:
@@ -29,7 +35,24 @@ class Logic:
 
         return Question(text, is_correct, explanation)
 
+    def generate_question(self):
+        """
+        Picks a question from question list and uses
+        counter as a question counter
+        :return: Question from questions list
+        """
+
+        return self.questions[self.counter].text
+
     def user_answer(self, answer_input):
+        """
+        Accepts answer_input as an argument, which is a user provided answer
+        for a question. Capitalizes the user provided answer, then check if it matches to
+        the answer we have in the csv file.
+
+        :return: "Out of tries!" if maximum tries are <= 0 otherwise prints "Not the correct answer, please try again!"
+        """
+
         capitalized_answer = answer_input.capitalize()
 
         if capitalized_answer == self.questions[self.counter].is_true:
@@ -37,6 +60,10 @@ class Logic:
             self.counter += 1
 
         else:
-            print("Not the correct answer, please try again! ")
+            self.max_misses -= 1
+            return "Out of tries!" if self.max_misses <= 0 else print(
+                f"Not the correct answer, you are {self.max_misses} tries left, please try again!")
 
-
+    def empty_line_check(self):
+        if self.counter >= len(self.questions) - 1:
+            return "Empty!"
